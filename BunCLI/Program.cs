3f477aa30ca391a23648910561399dfc5a643229
Tom Bloom <bloomtom@all8bits.com>
2018-11-21 00:48:10 -0500

    Add project files.

---
+++ BunCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BunCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
